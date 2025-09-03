d8770df143ad241388eb7f5e07043f6fd57eaf05
Virus Creative <dewiercoding@gmail.com>
2025-09-03 17:27:29 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// Force dark mode
 document.documentElement.classList.add('dark');
 
 createRoot(document.getElementById('root')!).render(
